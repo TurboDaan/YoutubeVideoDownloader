e43b5d528eaf09d57a2ad53b08d4dedd1e9aec72
TurboDaan_ <turbodaan12@gmail.com>
2024-04-30 22:29:57 +0200

    added code for downloading video

--- YoutubeVideoDownloader/MainForm.Designer.cs
+++ YoutubeVideoDownloader/MainForm.Designer.cs
@@ -32,9 +32,14 @@
             this.lblDownloadPath = new System.Windows.Forms.Label();
             this.pathDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.choosePathDialog = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBoxLink = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.comboBoxOptions = new System.Windows.Forms.ComboBox();
+            this.downloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,7 +56,7 @@
             // 
             this.lblDownloadPath.AutoSize = true;
             this.lblDownloadPath.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDownloadPath.Location = new System.Drawing.Point(12, 42);
+            this.lblDownloadPath.Location = new System.Drawing.Point(57, 42);
             this.lblDownloadPath.Name = "lblDownloadPath";
             this.lblDownloadPath.Size = new System.Drawing.Size(0, 20);
             this.lblDownloadPath.TabIndex = 1;
@@ -66,47 +71,105 @@
             this.choosePathDialog.UseVisualStyleBackColor = true;
             this.choosePathDialog.Click += new System.EventHandler(this.choosePathDialog_Click);
             // 
-            // textBox1
+            // txtBoxLink
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 209);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(538, 29);
-            this.textBox1.TabIndex = 3;
+            this.txtBoxLink.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLink.Location = new System.Drawing.Point(12, 150);
+            this.txtBoxLink.Name = "txtBoxLink";
+            this.txtBoxLink.Size = new System.Drawing.Size(538, 29);
+            this.txtBoxLink.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 176);
+            this.label2.Location = new System.Drawing.Point(8, 122);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.Size = new System.Drawing.Size(141, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Insert Youtube video link or playlist";
+            this.label2.Text = "Insert Youtube link";
             // 
-            // button1
+            // btnDownload
             // 
-            this.button1.Location = new System.Drawing.Point(556, 209);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 29);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Download";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDownload.Location = new System.Drawing.Point(556, 150);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(86, 29);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(356, 123);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(194, 20);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.Text = "Error: Idk somethin wrong";
+            this.lblMessage.Visible = false;
+            // 
+            // comboBoxOptions
+            // 
+            this.comboBoxOptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOptions.FormattingEnabled = true;
+            this.comboBoxOptions.Items.AddRange(new object[] {
+            "Video",
+            "Playlist"});
+            this.comboBoxOptions.Location = new System.Drawing.Point(155, 122);
+            this.comboBoxOptions.Name = "comboBoxOptions";
+            this.comboBoxOptions.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOptions.TabIndex = 7;
+            // 
+            // downloadProgressBar
+            // 
+            this.downloadProgressBar.Location = new System.Drawing.Point(470, 123);
+            this.downloadProgressBar.Name = "downloadProgressBar";
+            this.downloadProgressBar.Size = new System.Drawing.Size(172, 23);
+            this.downloadProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.downloadProgressBar.TabIndex = 8;
+            this.downloadProgressBar.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Path:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(486, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Made by TurboDaan_";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 255);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(654, 189);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.downloadProgressBar);
+            this.Controls.Add(this.comboBoxOptions);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnDownload);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBoxLink);
             this.Controls.Add(this.choosePathDialog);
             this.Controls.Add(this.lblDownloadPath);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Youtube Video Downloader";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,9 +182,14 @@
         private System.Windows.Forms.Label lblDownloadPath;
         private System.Windows.Forms.FolderBrowserDialog pathDialog;
         private System.Windows.Forms.Button choosePathDialog;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBoxLink;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ComboBox comboBoxOptions;
+        private System.Windows.Forms.ProgressBar downloadProgressBar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
