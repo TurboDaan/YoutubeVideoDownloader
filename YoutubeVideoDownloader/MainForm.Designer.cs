b91e81c973742dfcc3c90a5b49ec6bfa2aa2016f
TurboDaan_ <turbodaan12@gmail.com>
2024-04-30 20:39:24 +0200

    UI + Some code

---
+++ YoutubeVideoDownloader/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace YoutubeVideoDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDownloadPath = new System.Windows.Forms.Label();
+            this.pathDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.choosePathDialog = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Download Path (Default: Downloads folder):";
+            // 
+            // lblDownloadPath
+            // 
+            this.lblDownloadPath.AutoSize = true;
+            this.lblDownloadPath.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDownloadPath.Location = new System.Drawing.Point(12, 42);
+            this.lblDownloadPath.Name = "lblDownloadPath";
+            this.lblDownloadPath.Size = new System.Drawing.Size(0, 20);
+            this.lblDownloadPath.TabIndex = 1;
+            // 
+            // choosePathDialog
+            // 
+            this.choosePathDialog.Location = new System.Drawing.Point(338, 9);
+            this.choosePathDialog.Name = "choosePathDialog";
+            this.choosePathDialog.Size = new System.Drawing.Size(105, 23);
+            this.choosePathDialog.TabIndex = 2;
+            this.choosePathDialog.Text = "Choose Path";
+            this.choosePathDialog.UseVisualStyleBackColor = true;
+            this.choosePathDialog.Click += new System.EventHandler(this.choosePathDialog_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(538, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Insert Youtube video link or playlist";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 255);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.choosePathDialog);
+            this.Controls.Add(this.lblDownloadPath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDownloadPath;
+        private System.Windows.Forms.FolderBrowserDialog pathDialog;
+        private System.Windows.Forms.Button choosePathDialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ YoutubeVideoDownloader/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeVideoDownloader
+{
+    public partial class MainForm : Form
+    {
+        string defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
+        string downloadPath;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void choosePathDialog_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog pathDialog = new FolderBrowserDialog();
+            pathDialog.ShowDialog();                  
+
+            downloadPath = pathDialog.SelectedPath;
+            lblDownloadPath.Text = downloadPath;
+
+            if (downloadPath == "")
+                lblDownloadPath.Text = defaultPath;
+
+            // write the download path to a file
+            File.WriteAllText("path.txt", lblDownloadPath.Text);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("path.txt"))
+            {
+                downloadPath = File.ReadAllText("path.txt");
+                lblDownloadPath.Text = downloadPath;
+            }
+            else
+            lblDownloadPath.Text = defaultPath;
+            
+        }
+    }
+}
