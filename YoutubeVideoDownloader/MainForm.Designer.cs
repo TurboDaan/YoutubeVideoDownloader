bdc4bc6e78eaed5f16da798de4c408ad474c0777
TurboDaan_ <turbodaan12@gmail.com>
2024-05-01 09:57:22 +0200

    Audio UI option

--- YoutubeVideoDownloader/MainForm.Designer.cs
+++ YoutubeVideoDownloader/MainForm.Designer.cs
@@ -40,6 +40,12 @@
             this.downloadProgressBar = new System.Windows.Forms.ProgressBar();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comBoxFileType = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comBoxQuality = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -74,7 +80,7 @@
             // txtBoxLink
             // 
             this.txtBoxLink.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxLink.Location = new System.Drawing.Point(12, 150);
+            this.txtBoxLink.Location = new System.Drawing.Point(12, 138);
             this.txtBoxLink.Name = "txtBoxLink";
             this.txtBoxLink.Size = new System.Drawing.Size(538, 29);
             this.txtBoxLink.TabIndex = 3;
@@ -83,7 +89,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 122);
+            this.label2.Location = new System.Drawing.Point(8, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(141, 20);
             this.label2.TabIndex = 4;
@@ -91,7 +97,7 @@
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(556, 150);
+            this.btnDownload.Location = new System.Drawing.Point(556, 138);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(86, 29);
             this.btnDownload.TabIndex = 5;
@@ -103,7 +109,7 @@
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.Location = new System.Drawing.Point(356, 123);
+            this.lblMessage.Location = new System.Drawing.Point(417, 184);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(194, 20);
             this.lblMessage.TabIndex = 6;
@@ -117,16 +123,16 @@
             this.comboBoxOptions.Items.AddRange(new object[] {
             "Video",
             "Playlist"});
-            this.comboBoxOptions.Location = new System.Drawing.Point(155, 122);
+            this.comboBoxOptions.Location = new System.Drawing.Point(155, 106);
             this.comboBoxOptions.Name = "comboBoxOptions";
             this.comboBoxOptions.Size = new System.Drawing.Size(121, 21);
             this.comboBoxOptions.TabIndex = 7;
             // 
             // downloadProgressBar
             // 
-            this.downloadProgressBar.Location = new System.Drawing.Point(470, 123);
+            this.downloadProgressBar.Location = new System.Drawing.Point(160, 181);
             this.downloadProgressBar.Name = "downloadProgressBar";
-            this.downloadProgressBar.Size = new System.Drawing.Size(172, 23);
+            this.downloadProgressBar.Size = new System.Drawing.Size(197, 23);
             this.downloadProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.downloadProgressBar.TabIndex = 8;
             this.downloadProgressBar.Visible = false;
@@ -151,11 +157,75 @@
             this.label4.TabIndex = 10;
             this.label4.Text = "Made by TurboDaan_";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Download Progress:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(173, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Link Type:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(305, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "File Type:";
+            // 
+            // comBoxFileType
+            // 
+            this.comBoxFileType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxFileType.FormattingEnabled = true;
+            this.comBoxFileType.Location = new System.Drawing.Point(282, 106);
+            this.comBoxFileType.Name = "comBoxFileType";
+            this.comBoxFileType.Size = new System.Drawing.Size(121, 21);
+            this.comBoxFileType.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(417, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Video Quality:";
+            // 
+            // comBoxQuality
+            // 
+            this.comBoxQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxQuality.FormattingEnabled = true;
+            this.comBoxQuality.Location = new System.Drawing.Point(409, 106);
+            this.comBoxQuality.Name = "comBoxQuality";
+            this.comBoxQuality.Size = new System.Drawing.Size(121, 21);
+            this.comBoxQuality.TabIndex = 16;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 189);
+            this.ClientSize = new System.Drawing.Size(654, 210);
+            this.Controls.Add(this.comBoxQuality);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comBoxFileType);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.downloadProgressBar);
@@ -190,6 +260,12 @@
         private System.Windows.Forms.ProgressBar downloadProgressBar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comBoxFileType;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comBoxQuality;
     }
 }
 
